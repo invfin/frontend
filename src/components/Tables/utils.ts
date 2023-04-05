ad0326fcd13943c72de2af02928bc85abd89ab13
lucas <lucas.montes@essentialist.com>
2023-04-05 12:59:09 +0200

    create table with tooltip and link

--- src/components/Tables/utils.ts
+++ src/components/Tables/utils.ts
@@ -1,48 +1,59 @@
 import { StatementItem } from "@/api/company";
 
-type Column = { key: string; dataKey: string; title: string };
-type Row = { id: string; parentId: string; "column-field": string };
+export type Column = {
+  prop: string;
+  label: string;
+  fixed: boolean;
+  width: number;
+};
+export type Row = { fields: string; description?: string; link?: string };
 
 export function generateDataAndColumns(
   response: StatementItem
-): (Column[] | Row[])[] {
+): [Row[], Column[]] {
   const columns: Column[] = generateColumns(response);
   const data: Row[] = generateData(response);
-
   return [data, columns];
 }
 
 export function generateColumns(response: StatementItem): Column[] {
-  const fieldsColumn: Column = {
-    key: "column-field",
-    dataKey: "column-field",
-    title: `En ${response.currency}`
-  };
-  const columns = [fieldsColumn];
+  const fieldsColumn: Column = createColumn(
+    "fields",
+    `En ${response.currency}`,
+    true
+  );
+  const columns: Column[] = [fieldsColumn];
   response.labels.forEach(year => {
-    columns.push({
-      key: `column-${year}`,
-      dataKey: `column-${year}`,
-      title: year
-    });
+    columns.push(createColumn(year, year));
   });
   return columns;
 }
 
-function slugify(value: string): string {
-  return value.toLowerCase().replace(" ", "-");
+function createColumn(
+  prop: string,
+  label: string,
+  fixed = false,
+  width = 150
+): Column {
+  return {
+    prop: prop,
+    label: label,
+    fixed: fixed,
+    width: width
+  } as Column;
 }
+
 export function generateData(response: StatementItem): Row[] {
   const data: Row[] = [];
   response.fields.forEach(statementInfo => {
-    const row = {
-      id: slugify(statementInfo.title),
-      parentId: null,
-      "column-field": statementInfo.title
-    } as Row;
-    statementInfo.values.forEach((value, index) => {
-      const year = response.labels[index];
-      row[`column-${year}`] = value;
+    const row: Row = {
+      fields: statementInfo.title,
+      description: statementInfo.title,
+      link: statementInfo.url
+    };
+    statementInfo.values.forEach((value: number, index: number): void => {
+      const year: string = response.labels[index];
+      row[year] = value;
     });
     data.push(row);
   });
